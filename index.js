db048f006765db3618735ac77b3ec50666002b1b
Siyu Pan <pan0419@cs.washington.edu>
2019-10-02 17:08:12 -0700

    add princess window and collision logic

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const startButton = document.getElementsByClassName('play')[0];
+let adventurerWindow;
+let princessWindow;
 
 window.onload = () => {
     startButton.onclick = () => {
+        princessWindow = window.open("./princess/index.html",
+            "princessWindow",
+            `
+                menubar=no,
+                location=no,
+                status=1,
+                scrollbars=no,
+                width=226,
+                height=200,
+                screenX=${window.outerWidth},
+                screenY=0
+            `
+        );
+
         adventurerWindow = window.open("./adventurer/index.html", 
             "adventurerWindow", 
             `
@@ -15,5 +31,20 @@ window.onload = () => {
                 screenY=${window.outerHeight}
             `
         );
+
+        adventurerWindow.focus();
+        adventurerWindow.addEventListener("keydown", event => {
+            if (["w", "a", "s", "d"].includes(event.key)) {
+                if (checkCollision()) {
+                    // TODO: display ending
+                }
+            }
+        });
+
     }
+}
+
+const checkCollision = () => {
+    return adventurerWindow.screenX + adventurerWindow.outerWidth >= princessWindow.screenX
+        && adventurerWindow.screenY - adventurerWindow.outerHeight <= princessWindow.screenY;
 }
