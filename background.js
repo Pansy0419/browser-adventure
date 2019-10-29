f9f76e69a4332b92e80766351e79150b1d953588
Siyu Pan <pan0419@cs.washington.edu>
2019-10-29 15:09:28 -0700

    Add new tiles

--- background.js
+++ background.js
@@ -1,31 +1,26 @@
 let canvas;
 const path = new Array(9);
 for (var i = 0; i < path.length; i++) {
-  path[i] = new Array(16).fill(1);
+  path[i] = new Array(16).fill(0);
 }
 
-const getLevelContent = () => {
-  const levelType = params["level"].charAt(0);
-  const levelIndex = parseInt(params["level"].substring(1));
+/* Public functions */
 
-  if (levelType == "D") {
-    // Default levels
-    return LEVELS[levelIndex];
-  }
-};
-
+/**
+ * Draw level background onto canvas
+ */
 const drawBackground = () => {
-  const data = getLevelContent();
-  const lines = data.split(/\r?\n/);
-  console.log(data);
-  const asset = new Image();
   canvas = adventurerWindow.document.getElementsByTagName("canvas")[0];
   const ctx = canvas.getContext("2d");
   ctx.canvas.width = 1440;
   ctx.canvas.height = 864;
   ctx.imageSmoothingEnabled = false;
 
+  const data = getLevelContent();
+  const asset = new Image();
+
   asset.onload = function() {
+    const lines = data.split(/\r?\n/);
     for (const i in lines) {
       const tiles = lines[i].split(" ");
       for (const j in tiles) {
@@ -54,13 +49,14 @@ const drawBackground = () => {
     }
   };
   asset.src = "assets/background.png";
-
-  console.log(path);
 };
 
-const getFileData = file => {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.send(null);
-  return rawFile.responseText;
+const getLevelContent = () => {
+  const levelType = params["level"].charAt(0);
+  const levelIndex = parseInt(params["level"].substring(1));
+
+  if (levelType == "D") {
+    // Default levels
+    return LEVELS[levelIndex];
+  }
 };
