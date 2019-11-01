97f44862e2ee846870dc437a2a951e74bbe89c27
Siyu Pan <pan0419@cs.washington.edu>
2019-11-01 13:37:48 -0700

    add ending window

---
+++ ending/index.js
@@ -0,0 +1,20 @@
+const adventurer = document.getElementsByClassName("adventurer")[0];
+const princess = document.getElementsByClassName("princess")[0];
+const adventurerContainer = document.getElementsByClassName(
+  "character-container"
+)[0];
+
+window.onload = () => {
+  animateInDir(adventurer, "LEFT");
+
+  let delta = -110;
+  const animId = setInterval(() => {
+    if (delta == 40) {
+      clearInterval(animId);
+      clearAnimation(adventurer);
+    } else {
+      delta++;
+      adventurerContainer.style.marginLeft = delta + "px";
+    }
+  }, 20);
+};
--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const designerButton = document.getElementsByTagName("button")[1];
 const params = getParamsFromUrl();
 
 window.onload = () => {
+  const endingWindow = window.open(
+    "./ending/index.html",
+    "endingWindow",
+    `
+      menubar=no,
+      location=no,
+      status=1,
+      scrollbars=no,
+      width=380,
+      height=272,
+      screenX=${window.outerWidth},
+      screenY=0
+    `
+  );
   if (params["level"] != undefined) {
     // start game
     loadPrincess();
